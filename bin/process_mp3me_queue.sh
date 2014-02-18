48794b04015b8b5f8ca617295c4345287b806a01
Ernie Hershey <github@ernie.org>
2014-02-18 09:04:19 -0500

    start looking for metadata

--- bin/process_mp3me_queue.sh
+++ bin/process_mp3me_queue.sh
@@ -7,8 +7,17 @@ podcast_xml_path=/home/ernie/Dropbox/Web/ernie-podcast.xml
 for file in $incoming_dir/*
 do
   echo $file
+  if echo "$file" | grep -q \\.mp3$
+  then
+    # look for metadata
+    #
+    if ! [ -e "$file.metadata" ]
+    then
+    fi
+
+  fi
 done
 
 
 
-node generate_ernie_podcast_xml.js > $podcast_xml_path
+node `dirname $0`/generate_ernie_podcast_xml.js > $podcast_xml_path
