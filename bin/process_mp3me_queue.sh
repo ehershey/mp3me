2067134313412b24093d54718c3bb1a6204fb8ac
Ernie Hershey <ernie.hershey@10gen.com>
2014-02-23 21:06:45 -0500

    more file type guesses and a package.json

--- bin/process_mp3me_queue.sh
+++ bin/process_mp3me_queue.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
-incoming_dir=/home/ernie/Dropbox/Misc/mp3me/queue/incoming
+incoming_dir=$(ls -d /{home,Users}/ernie/Dropbox/Misc/mp3me/queue/incoming 2>/dev/null)
 
-podcast_xml_path=/home/ernie/Dropbox/Web/ernie-podcast.xml
+podcast_xml_path=$(ls -d /{home,Users}/ernie/Dropbox/Web/ 2>/dev/null)/ernie-podcast.xml
+
+if [ ! -e "$incoming_dir" ]
+then
+  echo "no incoming queue dir: $incoming_dir"
+  exit 2
+fi
+
 
 for file in $incoming_dir/*
 do
   echo $file
+  # already audio, probably ready to be listened
+  #
   if echo "$file" | grep -q \\.mp3$
   then
+    echo "Audio/mp3 file"
     # look for metadata
     #
-    if ! [ -e "$file.metadata" ]
-    then
-    fi
-
+  # already video, convert to audio
+  # 
+  elif echo "$file" | grep -q \\.mp4$
+  then
+    echo "Video/mp4 file"
+  # file with youtube URL
+  #
+  elif grep -q ^http.*youtube.com/ "$file"
+  then
+    echo "File containing URL"
   fi
 done
 
